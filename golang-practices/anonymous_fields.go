8b80d43240abc483f1a729aff10a4829e1256fcc
jacs4210 <jairo.cuartas18@gmail.com>
2021-08-18 19:54:56 -0500

    Metodos en campos anonimos con go

--- golang-practices/anonymous_fields.go
+++ golang-practices/anonymous_fields.go
@@ -6,6 +6,10 @@ type Human struct {
 	name string
 }
 
+func (this Human) hablar() string {
+	return "Bla, bla, bla"
+}
+
 type Dummy struct {
 	name string
 }
@@ -15,8 +19,13 @@ type Tutor struct {
 	Dummy
 }
 
+func (this Tutor) hablar() string {
+	return "Hello"
+}
+
 func main() {
 	tutor := Tutor{Human{"Jairo"}, Dummy{"Angie"}}
 
 	fmt.Println(tutor.Dummy.name)
+	fmt.Println(tutor.hablar())
 }
